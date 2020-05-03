50cf602bf76515d62bfd71fce0cad6e589354d9a
BloggeWorld <64740639+BloggeWorld@users.noreply.github.com>
2020-05-03 20:40:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <title>Blog Encryption</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
-<body id="page-top">
+<body>
  
  <h1>Encryption</h1>
  
